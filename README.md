385801829568af522e1c62435c2b5ab8c3d6d914
Hyeseong Kim <hey@hyeseong.kim>
2023-03-01 20:19:01 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# fastify-auth0-login
+
+A Fastify plugin for easily adding login feature via Auth0's [Authorization Code Flow](https://auth0.com/docs/get-started/authentication-and-authorization-flow/authorization-code-flow).
+
+## Prerequisites
+
+- Fasyify v4.x+
+- [@fastify/cookie](https://github.com/fastify/fastify-cookie)
+
+## Usage
+
+First, you need to create a Auth0 application.
+
+Confirm the `Domain`, `Client ID`, and `Client Secret` then set you application's `Login URL`, `Allowed Callback URL`, and `Allowd Web Origins`.
+
+For example to set for `https://localhost:3000`,
+
+![Example Callback URLs](https://user-images.githubusercontent.com/9696352/222122244-4f415985-c182-4953-8a82-e89885ff2213.png)
+
+Then configure your Fastify app with this plugin and `@fastify/cookie` like this:
+
+```
+import FastifyCookie from '@fastifyCookie';
+import FastifyAuth0Login from 'fastify-auth0-login';
+
+app.register(FastifyCookie, {
+  secret: COOKIE_SECRET,
+});
+
+app.register(FastifyAuth0Login, {
+  auth0: {
+    domin: YOUR_AUTH0_DOMAIN,
+    clientId: YOUR_AUTH0_CLIENT_ID,
+    clientSecret: YOUR_AUTH0_CLIENT_SECRET,
+  },
+  verifySession: (_req, sessionId) => {
+    return findSession(sessionId);
+  },
+  confirmSession: (_req, sessionId, idTokenClaims) => {
+    return findOrCreateSession(sessionId, idTokenClaims);
+  },
+});
+```
+
+Now you can initiate auth flow by GET `/auth/request`.
+
+```html
+<!-- This will redirect user to the Auth0 application's auth URL -->
+<a href="/auth/request">Login with Auth0</a>
+```
+
+## LICENSE
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,260 @@
+import { type FastifyPluginCallback, type FastifyRequest } from 'fastify';
+import fp from 'fastify-plugin';
+import type {} from '@fastify/cookie';
+import { createDecoder } from 'fast-jwt';
+
+export interface FastifySession {
+  id: string,
+  idSubject: string,
+}
+
+export type Auth0IdTokenClaims = {
+  nickname?: string;
+  name?: string;
+  email?: string;
+  email_verified?: boolean;
+  picture?: string;
+  updated_at: string;
+  iss: string;
+  sub: string;
+  aud: string;
+  iat: number;
+  exp: number;
+  sid: string;
+};
+
+export type Auth0Config = {
+  /**
+   * Domain for Auth0 Application
+   */
+  domain: string,
+
+  /**
+   * Client ID for Auth0 Application
+   */
+  clientId: string,
+
+  /**
+   * Client Secret for Auth0 Application
+   */
+  clientSecret: string,
+
+  /**
+   * OAuth 2.0 authorization scope
+   *
+   * @default "openid profile"
+   */
+  scope?: string,
+};
+
+export type FastifyAuth0LoginOptions = {
+  /**
+   * Fetch implementation
+   *
+   * @default globalThis.fetch
+   */
+  fetch?: typeof fetch,
+
+  /**
+   * A URL path to get home page
+   *
+   * @default "/"
+   */
+  userPath?: string,
+
+  /**
+   * A URL path to get login page
+   *
+   * @default "/login"
+   */
+  loginPath?: string,
+
+  /**
+   * A URL path to start the Authorization Code Flow via Auth0
+   *
+   * @default "/auth/request"
+   */
+  authRequestPath?: string,
+
+  /**
+   * A URL path to get Authorization Code Flow callback via Auth0
+   *
+   * @default "/auth/callback"
+   */
+  authCallbackPath?: string,
+
+  /**
+   * Name for Session ID
+   *
+   * @default "sessionId"
+   */
+  cookieName?: string,
+
+  /**
+   * A predicate function to determine whether the `currentSession` request hook
+   *
+   * @default () => true
+   */
+  requireSession?: (req: FastifyRequest) => boolean,
+
+  /**
+   * Find session 
+   *
+   * Plugin will attempts to create a new session if it returns null
+   */
+  verifySession: (req: FastifyRequest, sessionId: string) => Promise<FastifySession | null>,
+
+  /**
+   * Find or create session
+   *
+   * Authorization flow will be rejected if it returns null
+   *
+   * Note: It should also connect account with `idTokenClaims.sub` if it creates a new one.
+   */
+  confirmSession: (req: FastifyRequest, sessionId: string, idTokenClaims: Auth0IdTokenClaims) => Promise<FastifySession | null>,
+
+  /**
+   * Configuration for Auth0
+   */
+  auth0: Auth0Config,
+};
+
+const fastifyAuth0LoginPlugin: FastifyPluginCallback<FastifyAuth0LoginOptions> = (
+  fastify,
+  {
+    fetch,
+    userPath = '/',
+    loginPath = '/login',
+    authRequestPath = '/auth/request',
+    authCallbackPath = '/auth/callback',
+    cookieName = 'sessionId',
+    verifySession,
+    confirmSession,
+    requireSession = () => true,
+    auth0: auth0Config,
+  },
+  done,
+) => {
+  const decodeJWT = createDecoder();
+
+  const baseURL = new URL(`https://${auth0Config.domain}`);
+  const authorizeURL = new URL('/authorize', baseURL);
+  const tokenURL = new URL('/oauth/token', baseURL);
+
+  // Redirect to user's login request to the Auth0's Authorization Code Flow
+  fastify.route({
+    method: 'GET',
+    url: authRequestPath,
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          redirect_to: { type: 'string' },
+        },
+      },
+    },
+    async handler(req, reply) {
+      const query = req.query as { redirect_to?: string };
+      const self = new URL(authRequestPath, `${req.protocol}://${req.hostname}`);
+    
+      const redirectUrl = new URL(authCallbackPath, self);
+      if (query.redirect_to) {
+        redirectUrl.searchParams.set('redirect_to', query.redirect_to);
+      }
+      
+      const target = new URL(authorizeURL);
+      target.searchParams.set('response_type', 'code');
+      target.searchParams.set('client_id', auth0Config.clientId);
+      target.searchParams.set('redirect_uri', redirectUrl.toString());
+      target.searchParams.set('scope', auth0Config.scope || 'openid profile');
+
+      reply.redirect(307, target.toString());
+    },
+  })
+
+  // Callback for Auth0's Authorization Code Flow
+  fastify.route({
+    method: 'GET',
+    url: authCallbackPath,
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          code: { type: 'string' },
+          redirect_to: { type: 'string' },
+        },
+        required: ['code'],
+      },
+    },
+    async handler(req, reply) {
+      const query = req.query as { code: string; redirect_to?: string };
+      const self = new URL(authCallbackPath, `${req.protocol}://${req.hostname}`);
+
+      const response = await fetch(tokenURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          grant_type: 'authorization_code',
+          client_id: auth0Config.clientId,
+          client_secret: auth0Config.clientSecret,
+          code: query.code,
+          redirect_uri: self.toString(),
+        }),
+      });
+
+      const data = await response.json() as {
+        access_token: string;
+        id_token: string;
+        scope: string;
+        expires_in: number;
+        token_type: 'Bearer';
+      };
+      const decoded = decodeJWT(data.id_token) as Auth0IdTokenClaims;
+
+      const session = await confirmSession(req, decoded.sid, decoded);
+      if (!session) {
+        return reply.redirect(401, loginPath);
+      }
+
+      reply.setCookie(cookieName, session.id, { path: '/', expires: new Date(decoded.exp) });
+
+      if (query.redirect_to) {
+        reply.redirect(query.redirect_to);
+      } else {
+        reply.redirect(userPath);
+      }
+    },
+  });
+
+  fastify
+    .decorateRequest('currentSession', null)
+    .addHook('onRequest', async req => {
+      if (!requireSession(req)) {
+        return;
+      }
+      if (req.cookies[cookieName]) {
+        req.currentSession = await verifySession(req, req.cookies[cookieName]);
+      }
+    });
+
+  done();
+};
+
+export default fp(fastifyAuth0LoginPlugin, {
+  name: 'fastify-auth0-login',
+  fastify: '4.x',
+  dependencies: [
+    '@fastify/cookie',
+  ],
+  decorators: {
+    request: ['currentSession'],
+  },
+});
+
+declare module 'fastify' {
+  interface FastifyRequest {
+    currentSession: FastifySession | null;
+  }
+}
